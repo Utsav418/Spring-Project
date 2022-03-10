f696d98372ebdd45e47f056776541b39540a68cd
Utsav Kumar <utsavkumar9920@gmail.com>
2022-03-10 19:11:45 +0530

    github check

--- spring-demo-one/src/com/utsav/springdemo/GithubTest.java
+++ spring-demo-one/src/com/utsav/springdemo/GithubTest.java
@@ -1,5 +1,9 @@
 package com.utsav.springdemo;
 
 public class GithubTest {
-
+// System.out.println("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")
+	public String getDailyWorkOut() {
+		return "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";
+	}
+ 
 }
